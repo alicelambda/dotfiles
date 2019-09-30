6cdc2f1ad8995dbdbd02bca0375533c03254e2b0
Alice Reuter <alice@alicereuter.com>
2019-09-30 12:36:41 -0500

    added polybar

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ ln -sf ~/dotfiles/.emacs ~/.emacs
 ln -sf ~/dotfiles/.config/i3/config ~/.config/i3/config
 mkdir -p ~/.config/i3blocks
 ln -sf ~/dotfiles/.config/i3blocks/config ~/.config/i3blocks/config
-mkdir -p ~/.villefort
-ln -sf ~/dotfiles/villefort.hs ~/.villefort/villefort.hs
+mkdir -p ~/.config/polybar
+ln -sf ~/dotfiles/polybar/config ~/.config/polybar/config
 ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
 ln -sf ~/dotfiles/.xResources ~/.xResources
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
